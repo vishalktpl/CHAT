163d061cd7d6fd648c1a1746ab73a4b842ab422e
vishal.panchal <vishal.panchal@krishinc.com>
2015-12-28 16:53:47 +0530

    Test

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Magento_SampleNewPage',
+    'Dys_Wishlist',
     __DIR__
 );
