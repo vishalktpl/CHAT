3f0681eea652421fd95f6a1739b4235d73e55bf0
vishal.panchal <vishal.panchal@krishinc.com>
2015-12-28 16:49:38 +0530

    test

--- registration.php
+++ registration.php
@@ -6,6 +6,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Dys_Test',
+    'Magento_SampleNewPage',
     __DIR__
 );
