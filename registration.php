5594d7d7ae98071ebc157798954ce87baa2239f6
vishal.panchal <vishal.panchal@krishinc.com>
2015-12-29 15:05:09 +0530

    check

--- registration.php
+++ registration.php
@@ -1,8 +1,11 @@
 <?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Ktpl_Hello',
+    'Dys_Newsletterpopup',
     __DIR__
 );
-?>
